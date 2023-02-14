7ded7f4e3c565f5e9132df56fb4d2eb3759641f1
igig <dominik.teodorczyk1@gmail.com>
2023-02-14 21:30:02 +0100

    Added logging

--- ALPHA/api/client.py
+++ ALPHA/api/client.py
@@ -2,12 +2,17 @@ import requests as rq
 import os, json
 from websocket import create_connection
 from settings import *
+import logging
+
 
 
 class Client():
 
     def __init__(self, mode: str) -> None:
-        print(f'{mode} SESSION OPENED')
+
+        logging.basicConfig(filename='ALPHA\logging\client.log',level=logging.INFO, format='%(asctime)s.%(msecs)04d - %(levelname)s: %(message)s', datefmt='%d-%b-%y %H:%M:%S')
+        logging.info(f'{mode} SESSION OPENED')
+
         self.client_main_dir = os.path.dirname(os.path.abspath(__file__))
         if mode == 'REAL':
             self.user = UserREAL()
@@ -21,21 +26,28 @@ class Client():
     def connect(self):
         try:
             self.websocket_conection = create_connection(self.user.websocet)
-            return print(f'\033[0;32mConnected successfully to {self.user.websocet}\033[0m\n')
+            logging.info(f'Connected successfully to {self.user.websocet}')
+            print(f'Connected successfully to {self.user.websocet}')
         except:
-            return print(f'\033[0;31mNot connected to {self.user.websocet}\033[0m\n')
+            logging.error(f'Not connected to {self.user.websocet}')
+            print(f'Not connected to {self.user.websocet}')
 
     def disconnect(self):
         try:
             self.websocket_conection.close()
-            return print(f'\n\033[0;32mDisconnected successfully from {self.user.websocet}\033[0m\n')
+            logging.info(f'Disconnected successfully from {self.user.websocet}\n')
+            print(f'Disconnected successfully from {self.user.websocet}\n')
         except:
-            return print(f'\n\033[0;31mNot disconnected from {self.user.websocet}\033[0m\n')
+            logging.error(f'Not disconnected from {self.user.websocet}\n')
+            print(f'Not disconnected from {self.user.websocet}\n')
 
     def send(self, packet:dict):
-        self.websocket_conection.send(json.dumps(packet))
-        answer = self.websocket_conection.recv()
-        return json.loads(answer) 
+        try:
+            self.websocket_conection.send(json.dumps(packet))
+            answer = self.websocket_conection.recv()
+            return json.loads(answer) 
+        except:
+            pass
 
     def login(self):
         packet = {
@@ -44,15 +56,18 @@ class Client():
                 "userId": self.user.login,
                 "password": self.user.password
                 } 
-            }   
+            }
+        
         result = self.send(packet)
+
         if str(result["status"]) == 'True':
-            print(f'Login status: \033[0;32m{result["status"]}\033[0m \nStream session id: {result["streamSessionId"]}')
+            print(f'Login status: {result["status"]}, stream session id: {result["streamSessionId"]}')
             self.login_status = result["status"]
             self.stream_sesion_id = result["streamSessionId"]
+            logging.info(f'Logged as {self.user.login} (stream session id: {result["streamSessionId"]})')
         else:
-            print(f'Login status: \033[0;31m{result["status"]}\033[0m')
-
+            print(f'Login status: {result["status"]}')
+            logging.error(f'Not logged')
 
     def logout(self):
         packet = {
@@ -60,9 +75,11 @@ class Client():
             }   
         result = self.send(packet)
         if str(result["status"]) == 'True':
-            print(f'Logout status: \033[0;32m{result["status"]}\033[0m')
+            print(f'Logout status: {result["status"]}')
+            logging.info(f'Logged out of {self.user.login}')
         else:
-            print(f'Logout status: \033[0;31m{result["status"]}\033[0m')
+            print(f'Not logged out')
+            logging.error(f'Not logged out')
 
 
 def main():
