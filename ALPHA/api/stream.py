ba23726cf06ff00ed6fd40d21dcf0ee4d1298482
Dominik Teodorczyk <dominik.teodorczyk1@gmail.com>
2023-02-28 23:30:33 +0100

    Running stream of portfolio balance data

--- ALPHA/api/stream.py
+++ ALPHA/api/stream.py
@@ -8,29 +8,23 @@ class WalletStream():
     def __init__(self, api:object) -> None:
         self.api = api
         self.balance = None
-        print(self.api.stream_sesion_id)
-
-        self._t = Thread(target=self.stream(), args=())
-        self._t.setDaemon(True)
-        self._t.start()
-
-
 
     def subscribe(self):
-        self.api.send(json.dumps({"command": "getBalance", "streamSessionId": self.api.stream_sesion_id}))
-
+        self.api.stream_send({"command": "getBalance", "streamSessionId": self.api.stream_sesion_id})
 
     def streamread(self):
-        message = json.loads(self.api.websocket_conection.recv())
-        print(message)
+        message = json.loads(self.api.websocket_stream_conection.recv())
         try:
             if message['command'] == 'balance':
                 self.balance = np.fromiter(message['data'].values(), dtype=float)
                 print(self.balance)
         except:
             pass
+    
+    def keepalive():
+        self.api.stream_send({"command": "getBalance", "streamSessionId": self.api.stream_sesion_id})
 
     def stream(self):
         self.subscribe()
-        while self.api.connection == True:
+        while self.api.connection_stream == True:
             self.streamread()
