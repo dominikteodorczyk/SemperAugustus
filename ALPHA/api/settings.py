afd86e5d46c7051f7061980e545fc67544a96f47
Dominik Teodorczyk <dominik.teodorczyk1@gmail.com>
2023-03-01 22:03:14 +0100

    Change port reading from str to int

--- ALPHA/api/settings.py
+++ ALPHA/api/settings.py
@@ -9,8 +9,8 @@ class UserDEMO():
         self.login = getenv('XTB_LOGIN_DEMO')
         self.password = getenv('XTB_PASSWORD_DEMO')
         self.host = getenv('XTB_HOST_DEMO')
-        self.main_port = getenv('XTB_MAIN_PORT_DEMO')
-        self.streaming_port = getenv('XTB_STREAMING_PORT_DEMO')
+        self.main_port = int(getenv('XTB_MAIN_PORT_DEMO'))
+        self.streaming_port = int(getenv('XTB_STREAMING_PORT_DEMO'))
         self.websocet = getenv('XTB_WEBSOCET_DEMO')
         self.websocet_streaming_port = getenv('XTB_WEBSOCET_STRAMING_PORT_DEMO')
 
@@ -22,8 +22,8 @@ class UserREAL():
         self.login = getenv('XTB_LOGIN_REAL')
         self.password = getenv('XTB_PASSWORD_REAL')
         self.host = getenv('XTB_HOST_REAL')
-        self.main_port = getenv('XTB_MAIN_PORT_REAL')
-        self.streaming_port = getenv('XTB_STREAMING_PORT_REAL')
+        self.main_port = int(getenv('XTB_MAIN_PORT_REAL'))
+        self.streaming_port = int(getenv('XTB_STREAMING_PORT_REAL'))
         self.websocet = getenv('XTB_WEBSOCET_REAL')
         self.websocet_streaming_port = getenv('XTB_WEBSOCET_STRAMING_PORT_REAL')
 
