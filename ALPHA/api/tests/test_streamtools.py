fceb7f749f5aba6df98bba6162601ae6a503bd1a
Dominik Teodorczyk <dominik.teodorczyk1@gmail.com>
2023-03-09 13:19:41 +0100

    fixtures were created for the WalletStream class tests

--- ALPHA/api/tests/test_streamtools.py
+++ ALPHA/api/tests/test_streamtools.py
@@ -8,21 +8,31 @@ from streamtools import *
 
 
 class Test_WalletStream():
+      
 
+    # stream duration definition
     @pytest.fixture
-    def event():
+    def event_duration(self):
+        return 5
 
-        @stream_session_simulator
-        def wallet_stream():
-            return WalletStream()
-        
-        return wallet_stream()
-        
+    # definition of a stream data duration event using the WalletStream object under test 
+    # subscribing to and ad-reading the wallet balance data sent by the API using a decorator 
+    # that simulates stream sessions
+    @pytest.fixture
+    def event(self, event_duration):
+        # returns[0] = {api:api, balance:balance}, 
+        # returns[1] = args, 
+        # returns[2] = kwargs
+        return stream_session_simulator(event_duration)(WalletStream)
+    
+
+
+
+
         
     def test_walletstream_is_having_api(self, event):
-
         results = event()
-
+        assert type(results[0]['api']) is Client # api should be an object of class Client (passed to function from wrapper) 
 
 
 
