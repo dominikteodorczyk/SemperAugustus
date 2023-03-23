c12b64dcd28399f46bef09a24f836f4c0cd7aa51
Dominik Teodorczyk <dominik.teodorczyk1@gmail.com>
2023-03-23 14:20:31 +0100

    Fixed error writing 15M candles

--- ALPHA/api/streamtools.py
+++ ALPHA/api/streamtools.py
@@ -1,5 +1,6 @@
 import json
 import numpy as np
+from time import time
 
 class Stream():
 
@@ -61,34 +62,35 @@ class AssetBOX():
             dictor.pop('symbol')
             self.minute_1 = np.fromiter(dictor.values(), dtype=float).reshape(1,7)
             self.minute_1_5box = np.vstack([self.minute_1_5box, self.minute_1])
-            print('1MIN: ',self.minute_1,)
+            print(f'{time()} 1MIN: ',self.minute_1,)
 
 
     def make_more_candles(self):
-        if np.shape(self.minute_1_5box)[0] is 5:
+        if np.shape(self.minute_1_5box)[0] == 5:
             self.minute_5 = np.array([[
-                self.minute_1_5box[4,0],
+                self.minute_1_5box[-1,0],
                 self.minute_1_5box[0,1],
-                self.minute_1_5box[4,2],
+                self.minute_1_5box[-1,2],
                 np.max(self.minute_1_5box[:,3]),
                 np.min(self.minute_1_5box[:,4]),
                 self.minute_1_5box[:,5].sum(),
                 self.minute_1_5box[-1,6]]])
             self.minute_1_5box = np.empty(shape=[0, 7])
             self.minute_5_15box = np.vstack([self.minute_5_15box, self.minute_5])
-            print('5MIN:',self.minute_5)
+            print(f'{time()} 5MIN:',self.minute_5)
+
         
-        if np.shape(self.minute_5_15box)[0] is 3:
+        if np.shape(self.minute_5_15box)[0] == 3:
             self.minute_15 = np.array([[
-                self.minute_5[2,0],
-                self.minute_5[0,1],
-                self.minute_5[2,2],
-                np.max(self.minute_5[:,3]),
-                np.min(self.minute_5[:,4]),
-                self.minute_5[:,5].sum(),
-                self.minute_5[-1,6]]])
+                self.minute_5_15box[-1,0],
+                self.minute_5_15box[0,1],
+                self.minute_5_15box[-1,2],
+                np.max(self.minute_5_15box[:,3]),
+                np.min(self.minute_5_15box[:,4]),
+                self.minute_5_15box[:,5].sum(),
+                self.minute_5_15box[-1,6]]])
             self.minute_5_15box = np.empty(shape=[0, 7])
-            print('15MIN:',self.minute_15)
+            print(f'{time()} 15MIN: ',self.minute_15)
 
     def stream(self):
         self.subscribe()
