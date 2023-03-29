52363fb16843f4988148d9cb66e73159980dd3ca
Dominik Teodorczyk <dominik.teodorczyk1@gmail.com>
2023-03-29 23:26:29 +0200

    change in login time formatting

--- beta/utils/setup_loger.py
+++ beta/utils/setup_loger.py
@@ -1,13 +1,15 @@
 import logging
 
 def setup_logger(name, log_file, level_of_log=logging.INFO, print = False):
+
     message_format = logging.Formatter(
-        '%(asctime)s %(levelname)s %(message)s'
+        '%(asctime)s.%(msecs)04d - %(levelname)s: %(message)s', 
+        '%Y-%m-%d %H:%M:%S'
     )
     
     file_handler = logging.FileHandler(log_file)
     file_handler.setFormatter(message_format)
-    
+
     logger = logging.getLogger(name)
     logger.setLevel(level_of_log)
     logger.addHandler(file_handler)
