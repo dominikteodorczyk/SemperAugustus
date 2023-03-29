c009f84f2df668ba19428bdd56a7860fdf8e9beb
Dominik Teodorczyk <dominik.teodorczyk1@gmail.com>
2023-03-29 22:21:05 +0200

    Added function to define logger

---
+++ beta/utils/setup_loger.py
@@ -0,0 +1,19 @@
+import logging
+
+def setup_logger(name, log_file, level_of_log=logging.INFO, print = False):
+    message_format = logging.Formatter(
+        '%(asctime)s %(levelname)s %(message)s'
+    )
+    
+    file_handler = logging.FileHandler(log_file)
+    file_handler.setFormatter(message_format)
+    
+    logger = logging.getLogger(name)
+    logger.setLevel(level_of_log)
+    logger.addHandler(file_handler)
+
+    if print is True:
+        print_handler = logging.StreamHandler()
+        logger.addHandler(print_handler)
+
+    return logger
