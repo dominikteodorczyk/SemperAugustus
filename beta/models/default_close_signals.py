873377cfd9fe9463b898246472636f5878fa0619
Dominik Teodorczyk <dominik.teodorczyk1@gmail.com>
2023-04-04 00:54:38 +0200

    structural changes and added read functions

--- beta/models/default_close_signals.py
+++ beta/models/default_close_signals.py
@@ -1,56 +1,97 @@
 from api.streamtools import *
+from api.commands import *
+from utils.setup_loger import setup_logger
 
-class DefaultCloseSignal():
+class DefaultCloseSignal:
+    def __init__(self):
+        self.DCS_logger = setup_logger(
+            "DCS_logger", "beta\log\DCS_logger.log"
+        )
 
-    def __init__(self,api,open_price, stop_loss_lvl:float, position_margin:float):
-        self.open_price = open_price
-        self.sl_lvl = stop_loss_lvl
-        self.margin = position_margin
-        self.close_signal = False
+    def init(
+            self, api: object, position_data:dict, sl_start: float = 0.4, tp_min: float = 0.5, 
+            tp_max: float = 0.1,):
+        self.api =api
+        self.symbol = position_data["transactions_data"]["symbol"]
+        self.order = position_data["order_no"]
+        self.position = position_data["transactions_data"]["position"]
+        self.open_price = position_data["transactions_data"]["cmd"]
+        self.margin = position_data["margin"]
+        self.cmd = position_data["transactions_data"]["cmd"]
 
-        self.current_price = None
-        self.current_wallet_banace = None
-        self.current_return = None
-        self.minute_1 = None
-        self.minutes_5 = None
-        self.minutes_15 = None
-        
-        self.walet_stream = WalletStream(api=api)
-        self.price_data = PositionObservator(api=api,symbol=self.symbol)
+        self.sl_start = sl_start
+        self.tp_min = tp_min
+        self.tp_max = tp_max
+
+        self.walet_stream = WalletStream(api=self.api)
+        self.price_data = PositionObservator(api=api, symbol=self.symbol,order_no=self.order)
         self.status_to_close = False
+
+        if self.cmd == 1:
+            self.multiplier_value = 1
+        else:
+            self.multiplier_value = -1
 
     def subscribe_data(self):
         self.walet_stream.subscribe()
         self.price_data.subscribe()
 
     def read_data(self):
-        self.current_wallet_banace = self.walet_stream.balance
-        self.current_price = self.price_data.curent_price
-        self.current_return = self.price_data.profit
-        self.minute_1 = self.price_data.minute_1
-        self.minutes_5 = self.price_data.minute_5
-        self.minutes_15 = self.price_data.minute_15
+        self.walet_stream.streamread()
+        self.price_data.streamread()
 
     def sefault_close_position(self):
         pass
 
-    def run_position(self):
+    def run(self):
         self.subscribe_data()
         while self.status_to_close is False:
-            self.walet_stream.streamread()
-            self.price_data.streamread()
             self.read_data()
+            if self.price_data.minute_15.any():
+                self.DCS_logger.info(
+                f"Candle core: {self.price_data.minute_15[0,2] - self.price_data.minute_15[0,1]}"
+                )
+            elif self.price_data.minute_5.any():
+                self.DCS_logger.info(
+                f"Candle core: {self.price_data.minute_5[0,2] - self.price_data.minute_5[0,1]}"
+                )
+            elif self.price_data.minute_1.any():
+                self.DCS_logger.info(
+                f"Candle core: {self.price_data.minute_1[0,2] - self.price_data.minute_1[0,1]}"
+                )
+            else:
+                # self.first_stage_monitor()
+                pass
 
-    def first_stage_monitor(self, actual_profit):
-        if (actual_profit/self.margin) < self.sl_lvl:
-            self.close_signal = True
-            return self.close_signal
-        else:
-            self.close_signal = False
-            pass
+            # if self.status_to_close == True:
+            #     close_position(api=self.api)
 
-    def main_stage(self):
-        self.close_signal = True
-        return self.close_signal
-    
 
+
+
+    # def first_stage_monitor(self):
+    #     if (self.price_data.profit / self.margin) < self.sl_start :
+    #         self.close_signal = True
+    #         return self.close_signal
+    #     else:
+    #         self.close_signal = False
+    #         pass
+
+    # def get_1M_candle_dir(self):
+    #     return self.price_data.minute_1[0, 2] - self.price_data.minute_1[0, 1]
+
+    # def take_profit(self, candle):
+    #     time_to_candle_end = (
+    #         60000 - (self.price_data.curent_price[0, 6] - (candle[0, 0]) - 60000)
+    #     ) / 1000
+
+    #     max_tp_price = candle[0, 1] + (candle[0, 2] - candle[0, 1]) * self.tp_max
+    #     min_tp_price = candle[0, 1] + (candle[0, 2] - candle[0, 1]) * self.tp_min
+
+
+    #     if self.price_data.minute_1[0, 2] - self.price_data.minute_1[0, 1] > 0 and self.cmd :
+            
+            
+        
+
+    #         return self.close_signal
