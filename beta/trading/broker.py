6ccb9cd8f8c5fd2e808b395708cb5af990516d34
Dominik Teodorczyk <dominik.teodorczyk1@gmail.com>
2023-04-04 00:55:19 +0200

    fixed way of close signal object init

--- beta/trading/broker.py
+++ beta/trading/broker.py
@@ -1,8 +1,9 @@
 from api.client import Client
 from api.streamtools import WalletStream, PositionObservator
-from api.commands import buy_transaction, sell_transaction
+from api.commands import buy_transaction, sell_transaction, close_position
 from threading import Thread
 from models.default_close_signals import *
+from time import sleep
 
 
 class TradingSlot():
@@ -16,7 +17,7 @@ class Position():
 
     def __init__(
             self, api, cmd, symbol: str, 
-            volume:float, close_signal:object = DefaultCloseSignal()):
+            volume:float, close_signal:object = None):
         self.api = api
         self.symbol = symbol
         self.volume = volume
@@ -27,45 +28,47 @@ class Position():
             self.order = buy_transaction(
                 api=self.api,symbol=self.symbol,volume=self.volume
             )
+            self.close_signal.init(api=self.api,position_data=self.order)
             self.close_signal.run()
         if self.cmd == 1:
             self.order = sell_transaction(
                 api=self.api,symbol=self.symbol,volume=self.volume
             )
+            self.close_signal.init(api=self.api,position_data=self.order)
             self.close_signal.run()
 
-        self.current_price = None
-        self.current_wallet_banace = None
-        self.current_return = None
-        self.minute_1 = None
-        self.minutes_5 = None
-        self.minutes_15 = None
+    #     self.current_price = None
+    #     self.current_wallet_banace = None
+    #     self.current_return = None
+    #     self.minute_1 = None
+    #     self.minutes_5 = None
+    #     self.minutes_15 = None
         
-        self.walet_stream = WalletStream(api=api)
-        self.price_data = PositionObservator(api=api,symbol=self.symbol)
-        self.status_to_close = False
+    #     self.walet_stream = WalletStream(api=api)
+    #     self.price_data = PositionObservator(api=api,symbol=self.symbol)
+    #     self.status_to_close = False
 
-    def subscribe_data(self):
-        self.walet_stream.subscribe()
-        self.price_data.subscribe()
+    # def subscribe_data(self):
+    #     self.walet_stream.subscribe()
+    #     self.price_data.subscribe()
 
-    def read_data(self):
-        self.current_wallet_banace = self.walet_stream.balance
-        self.current_price = self.price_data.curent_price
-        self.current_return = self.price_data.profit
-        self.minute_1 = self.price_data.minute_1
-        self.minutes_5 = self.price_data.minute_5
-        self.minutes_15 = self.price_data.minute_15
+    # def read_data(self):
+    #     self.current_wallet_banace = self.walet_stream.balance
+    #     self.current_price = self.price_data.curent_price
+    #     self.current_return = self.price_data.profit
+    #     self.minute_1 = self.price_data.minute_1
+    #     self.minutes_5 = self.price_data.minute_5
+    #     self.minutes_15 = self.price_data.minute_15
 
-    def sefault_close_position(self):
-        pass
+    # def sefault_close_position(self):
+    #     pass
 
-    def run_position(self):
-        self.subscribe_data()
-        while self.status_to_close is False:
-            self.walet_stream.streamread()
-            self.price_data.streamread()
-            self.read_data()
+    # def run_position(self):
+    #     self.subscribe_data()
+    #     while self.status_to_close is False:
+    #         self.walet_stream.streamread()
+    #         self.price_data.streamread()
+    #         self.read_data()
 
 
     #TODO: całość powyższa powinna odbywać się w close signal i to 
@@ -93,7 +96,7 @@ class TradingSession():
 def position():
     api = Client('DEMO')
     api.open_session()
-    position_data = sell_transaction(api=api,symbol='BITCOIN',volume=0.01)
-    print(position_data)
-    PositionObservator(api=api, symbol='BITCOIN', order_no=position_data['order_no']).stream()
+    Position(
+        api=api,cmd=1,symbol='EURUSD',volume=0.01,
+        close_signal=DefaultCloseSignal())
     api.close_session()
