1cc19bdbd28a1e1d902be8b624d3fd8e4b994735
Dominik <dominik.teodorczyk1@gmail.com>
2023-06-04 22:37:22 +0200

    wallet perform update

--- src/utils/wallet.py
+++ src/utils/wallet.py
@@ -1,10 +1,23 @@
-from time import sleep
-import logging
+"""
+Module containing portfolio definitions and algorithms that manage risk
+"""
+
+
 
 class Wallet():
+    """
+    Portfolio object class, calculating risk and performing money management.
+    """
+
     def __init__(self) -> None:
-        self.testwallet = 1
         pass
 
-    def run(self):
+
+class WalletPerform():
+    """
+    A class of object that calculates portfolio performance over time
+    and risk statistics.
+    """
+
+    def __init__(self) -> None:
         pass
