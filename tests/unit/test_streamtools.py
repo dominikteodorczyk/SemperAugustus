3e69988fa9b8dd4a0132e5e89355980c52d8fffd
Dominik <dominik.teodorczyk1@gmail.com>
2023-06-19 21:30:04 +0200

    added balance reading and writing tests

--- tests/unit/test_streamtools.py
+++ tests/unit/test_streamtools.py
@@ -3,6 +3,7 @@ Check the XTBClient of XTB API is valid.
 """
 
 import pytest
+from unittest.mock import MagicMock
 import numpy as np
 from api.client import XTBClient, stream_session_simulator
 from api.streamtools import WalletStream
@@ -24,9 +25,15 @@ class Test_WalletStream:
         # returns[2] = kwargs
         return stream_session_simulator(event_duration)(WalletStream)
 
+    @pytest.fixture
+    def mock_xtb_client(self):
+        # FIXME: Tworzenie mocka dla XTBClient
+        client = MagicMock()
+        yield client
+
     def test_walletstream_is_having_api(self, event):
-        results = event()
-        assert type(results[0]["client"]) is XTBClient
+        object = WalletStream()
+        assert type(object.client) is XTBClient
         # api should be an object of class XTBClient
         # (passed to functionfrom wrapper)
 
@@ -48,6 +55,113 @@ class Test_WalletStream:
             np.shape(results[0]["balance"])[0] == 10
         )  # API will send a portfolio balance consisting of 10 parameters
 
+    def test_walletstream_is_writing_message_to_balance_array(self, mock_xtb_client):
+        # Creating a WalletStream instance with a mock XTBClient
+        wallet_stream = WalletStream()
+        wallet_stream.client = mock_xtb_client
+        # Preparing data to be returned by the mock XTBClient.stream_read method
+        mock_message = {
+            'command': 'balance',
+            'data': {
+                'balance': 9992.68,
+                'margin': 148.08,
+                'equityFX': 9992.23,
+                'equity': 9992.23,
+                'marginLevel': 6747.86,
+                'marginFree': 9844.15,
+                'credit': 0.0,
+                'stockValue': 0.0,
+                'stockLock': 0.0,
+                'cashStockValue': 0.0
+            }
+        }
+        # Setting the behavior of mock XTBClient.read_stream
+        wallet_stream.client.stream_read.return_value = mock_message
+        # Calling the read_stream method
+        wallet_stream.read_stream()
+        # Checking whether the balance stream message was processed correctly
+        expected_balance = np.array(
+            [9992.68, 148.08, 9992.23, 9992.23, 6747.86, 9844.15, 0.0, 0.0, 0.0, 0.0]
+        )
+        assert np.array_equal(wallet_stream.balance, expected_balance)
+
+    def test_walletstream_is_not_writing_message_to_balance(self, mock_xtb_client):
+        # Test whether the read_strem method writes balance-only data
+        # Creating a WalletStream instance with a mock XTBClient
+        wallet_stream = WalletStream()
+        wallet_stream.client = mock_xtb_client
+        # Preparing data to be returned by the mock XTBClient.stream_read method
+        mock_message = {
+            "command": "candle",
+            "data": {
+                "close": 4.1849,
+                "ctm": 1378369375000,
+                "ctmString": "Sep 05, 2013 10:22:55 AM",
+                "high": 4.1854,
+                "low": 4.1848,
+                "open": 4.1848,
+                "quoteId": 2,
+                "symbol": "EURUSD",
+                "vol": 0.0
+            }
+        }
+        # Setting the behavior of mock XTBClient.read_stream
+        wallet_stream.client.stream_read.return_value = mock_message
+        # Calling the read_stream method
+        wallet_stream.read_stream()
+        # Checking whether the balance stream message was processed correctly
+        expected_balance = np.array([])
+        assert np.array_equal(wallet_stream.balance, expected_balance)
+
+    def test_walletstream_is_overwriting_balance_while_stream(self, mock_xtb_client):
+        # Test whether the read_strem method writes balance-only data
+        # Creating a WalletStream instance with a mock XTBClient
+        wallet_stream = WalletStream()
+        wallet_stream.client = mock_xtb_client
+        # Preparing data to be returned by the mock XTBClient.stream_read method
+        mock_message = [
+        {
+            'command': 'balance',
+            'data': {
+                'balance': 9992.68,
+                'margin': 148.08,
+                'equityFX': 9992.23,
+                'equity': 9992.23,
+                'marginLevel': 6747.86,
+                'marginFree': 9844.15,
+                'credit': 0.0,
+                'stockValue': 0.0,
+                'stockLock': 0.0,
+                'cashStockValue': 0.0
+            }
+        },
+        {
+            'command': 'balance',
+            'data': {
+                'balance': 9998.68,
+                'margin': 146.08,
+                'equityFX': 9942.23,
+                'equity': 9292.23,
+                'marginLevel': 6767.86,
+                'marginFree': 9843.15,
+                'credit': 1.24,
+                'stockValue': 0.0,
+                'stockLock': 0.0,
+                'cashStockValue': 12.0
+            }
+        }
+    ]
+        # Setting the behavior of mock XTBClient.read_stream
+        wallet_stream.client.stream_read.side_effect = mock_message
+        # Calling the read_stream method
+        wallet_stream.stream()
+        # Checking that the open_session and subscribe methods were called correctly
+        wallet_stream.client.open_session.assert_called_once()
+        # Checking whether the balance stream message was processed correctly
+        expected_balance = np.array(
+            [9998.68, 146.08, 9942.23, 9292.23, 6767.86, 9843.15, 1.24, 0.0, 0.0, 12.0]
+        )
+        assert np.array_equal(wallet_stream.balance, expected_balance)
 
 # TODO: use the following tests to cosntruct the others.
 # @pytest.mark.skip(reason="Class withdrawn / to be rebuilt")
