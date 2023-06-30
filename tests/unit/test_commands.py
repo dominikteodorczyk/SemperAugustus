4ab95989e6c6c7d0b0852e6f78cc1920376c2fde
Dominik <dominik.teodorczyk1@gmail.com>
2023-06-30 22:58:24 +0200

    update

---
+++ tests/unit/test_commands.py
@@ -0,0 +1,14 @@
+"""
+Check the XTBClient of XTB API is valid.
+"""
+
+import pytest
+from unittest.mock import Mock, MagicMock, patch
+import pytest
+import numpy as np
+from api.commands import get_trades
+
+class Test_get_trades():
+
+    def test_its_get_trades_return_dict():
+        pass
