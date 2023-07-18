674a19ce6cd79f956f401c1e27fd35b11cf45450
Dominik <dominik.teodorczyk1@gmail.com>
2023-07-18 22:08:58 +0200

    init db client object

--- src/data/menager.py
+++ src/data/menager.py
@@ -1,3 +1,9 @@
 '''
 Menager danych umożliwiający zasilanie oraz pobieranie danych historycznych
 '''
+
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String
+
+class DataBaseClient:
+
+    def __init__(self) -> None:
