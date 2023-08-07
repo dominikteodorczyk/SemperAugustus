0abe2c34c55c63ad7362b46aed85b02a1297972e
Dominik <dominik.teodorczyk1@gmail.com>
2023-08-07 22:23:28 +0200

    db fixed

--- src/utils/dbtools.py
+++ src/utils/dbtools.py
@@ -11,23 +11,44 @@ from sqlalchemy import (
     MetaData,
     Table,
     inspect,
+    insert,
 )
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 from settings import DataBases
 
 
-class TrasactionRepository:
+def create_specific_transaction_save_model(table_name):
+    class TrasactionRecord(declarative_base()):
+        __tablename__ = table_name
+        position = Column(
+            "position", Integer, primary_key=True, nullable=False
+        )
+        symbol = Column("symbol", String)
+        order = Column("order", Integer)
+        cmd = Column("cmd", Integer)
+        volume = Column("volume", Float)
+        profit = Column("profit", Float)
+        open_price = Column("open_price", Float)
+        open_time = Column("open_time", Integer)
+        close_price = Column("close_price", Float)
+        close_time = Column("close_time", Integer)
+
+    return TrasactionRecord
+
+
+class TrasactionSave:
     def __init__(self, symbol):
         self.symbol = symbol
         self.engine = create_engine(f"sqlite:///{DataBases().transactions}")
-        self.session = sessionmaker(bind=self.engine)
+        self.session = sessionmaker(bind=self.engine)()
+        self.metadata = MetaData()
+        self.model = create_specific_transaction_save_model(self.symbol)
         self.check_n_create()
 
     def create_symbol_table(self):
-        metadata = MetaData()
         Table(
             self.symbol,
-            metadata,
+            self.metadata,
             Column("position", Integer, primary_key=True, nullable=False),
             Column("symbol", String),
             Column("order", Integer),
@@ -39,7 +60,24 @@ class TrasactionRepository:
             Column("close_price", Float),
             Column("close_time", Integer),
         )
-        metadata.create_all(self.engine)
+        self.metadata.create_all(self.engine)
 
     def check_n_create(self):
-        if not inspect(self.engine).has_table(self.symbol): self.create_symbol_table()
+        if not inspect(self.engine).has_table(self.symbol):
+            self.create_symbol_table()
+
+    def add(self, trade):
+        transaction = self.model(
+            position=trade["position"],
+            symbol=trade["symbol"],
+            order=trade["order"],
+            cmd=trade["cmd"],
+            volume=trade["volume"],
+            profit=trade["profit"],
+            open_price=trade["open_price"],
+            open_time=trade["open_time"],
+            close_price=trade["close_price"],
+            close_time=trade["close_time"],
+        )
+        self.session.add(transaction)
+        self.session.commit()
