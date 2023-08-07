4af3e5131ded8842e67a01fe85402f159a467b92
Dominik <dominik.teodorczyk1@gmail.com>
2023-08-08 00:04:48 +0200

    syntax and docstring fixing

--- src/utils/dbtools.py
+++ src/utils/dbtools.py
@@ -17,10 +17,26 @@ from settings import DataBases
 
 
 def create_specific_transaction_save_model(table_name: str):
+    """
+    Creates a specific data model for existing tables that conform
+    to the symbol.
+
+    Returns:
+        model object
+    """
+
     class Base(DeclarativeBase):
-        pass
+        """
+        The Declarative Mapping is the typical way that mappings are
+        constructed in modern SQLAlchemy. More:
+        https://docs.sqlalchemy.org/en/20/orm/
+        """
 
     class TrasactionRecord(Base):
+        """
+        Transaction data model  together with the defined table name.
+        """
+
         __tablename__ = table_name
         position = Column(
             "position", Integer, primary_key=True, nullable=False
@@ -39,7 +55,27 @@ def create_specific_transaction_save_model(table_name: str):
 
 
 class TrasactionSave:
-    def __init__(self, symbol):
+    """
+    A class representing a transaction saver.
+
+    This class provides functionality to save transactions for a given symbol
+    into a SQLite database using SQLAlchemy.
+
+    Args:
+        symbol (str): The symbol associated with the transactions.
+
+    Attributes:
+        symbol (str): The symbol associated with the transactions.
+        engine (Engine): The SQLAlchemy engine object for the database
+            connection.
+        session (Session): The SQLAlchemy session object for database
+            interactions.
+        metadata (MetaData): The SQLAlchemy metadata object for table
+            and schema definitions.
+
+    """
+
+    def __init__(self, symbol: str):
         self.symbol = symbol
         self.engine = create_engine(
             f"sqlite:///{DataBases().transactions}", echo=True
@@ -50,6 +86,9 @@ class TrasactionSave:
         self.check_n_create()
 
     def create_symbol_table(self):
+        """
+        Method that creates table.
+        """
         Table(
             self.symbol,
             self.metadata,
@@ -67,10 +106,36 @@ class TrasactionSave:
         self.metadata.create_all(self.engine)
 
     def check_n_create(self):
+        """
+        This method checks if a table with the specified symbol name exists
+        in the database. If not, it creates the table using the defined
+        metadata.
+        """
         if not inspect(self.engine).has_table(self.symbol):
             self.create_symbol_table()
 
     def add(self, trade):
+        """
+        This method adds a new trade record to the database for the
+        specified symbol. The trade information is passed as a dictionary
+        containing the trade details.
+
+        Args:
+            trade (dict): A dictionary containing trade details with
+                the following keys:
+                - "position" (int):  The position number of the trade.
+                - "symbol" (str): The symbol associated with the trade.
+                - "order" (int): The order number of the trade.
+                - "cmd" (int): The command identifier for the trade.
+                - "volume" (float): The volume of the trade.
+                - "profit" (float): The profit earned from the trade.
+                - "open_price" (float): The opening price of the trade.
+                - "open_time" (int): The timestamp (Unix epoch) when the
+                    trade was opened.
+                - "close_price" (float): The closing price of the trade.
+                - "close_time" (int): The timestamp (Unix epoch) when the
+                    trade was closed.
+        """
         model = create_specific_transaction_save_model(self.symbol)
         transaction = model(
             position=trade["position"],
